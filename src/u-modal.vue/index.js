90a3535f0b52dd19c38ed038c21411b013356004
Rainfore <rainforest92@126.com>
2018-09-14 22:55:55 +0800

    :sparkles: Add two options in <u-modal>

--- src/u-modal.vue/index.js
+++ src/u-modal.vue/index.js
@@ -1,3 +1,10 @@
 import Modal from 'cloud-ui.vusion/src/u-modal.vue';
 
-export default Modal;
+export default {
+    name: 'u-modal',
+    extends: Modal,
+    props: {
+        primaryButton: { type: String, default: 'okButton' },
+        disablePrimaryButton: { type: Boolean, default: false },
+    },
+};
